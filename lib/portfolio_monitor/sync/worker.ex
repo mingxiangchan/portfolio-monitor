ada9fe1b37ca3d7aa7312dc0efcacdabd5e4fab2
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-01-22 14:32:58 +0800

    add bitmex acc channel and broadcast updates to it

--- lib/portfolio_monitor/sync/worker.ex
+++ lib/portfolio_monitor/sync/worker.ex
@@ -1,37 +1,27 @@
 defmodule PortfolioMonitor.Sync.Worker do
   use ExBitmex.WebSocket
   alias PortfolioMonitor.Portfolio
+  alias PortfolioMonitorWeb.Endpoint
 
   def handle_response(json, state) do
     # spawn async, no-link process to perform db insert
     # it is acceptable to lose inserts
     Task.start(fn ->
-      insert_into_db(json, state[:acc_id])
+      persist_response(json, state[:acc_id])
     end)
   end
 
-  def insert_into_db(%{"action" => "partial"}, _), do: nil
+  def persist_response(%{"action" => "partial"}, _), do: nil
 
-  def insert_into_db(%{"table" => "position"} = json, acc_id) do
-    Portfolio.create_position(%{
-      data: json["data"],
-      bitmex_acc_id: acc_id
-    })
+  def persist_response(%{"table" => table, "data" => data}, acc_id) do
+    Endpoint.broadcast("bitmex_acc" <> acc_id, table, data)
+
+    changes = %{data: data, bitmex_acc_id: acc_id}
+
+    case table do
+      "position" -> Portfolio.create_position(changes)
+      "margin" -> Portfolio.create_margin(changes)
+      "order" -> Portfolio.create_order_detail(changes)
+    end
   end
-
-  def insert_into_db(%{"table" => "margin"} = json, acc_id) do
-    Portfolio.create_margin(%{
-      data: json["data"],
-      bitmex_acc_id: acc_id
-    })
-  end
-
-  def insert_into_db(%{"table" => "order"} = json, acc_id) do
-    Portfolio.create_order_detail(%{
-      data: json["data"],
-      bitmex_acc_id: acc_id
-    })
-  end
-
-  def insert_into_db(_, _), do: nil
 end
