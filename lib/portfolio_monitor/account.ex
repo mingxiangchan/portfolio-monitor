28f25367fc60c58e4aa64326cd43abab6536833f
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-01-31 15:14:40 +0800

    jsonb query to handle availableBalance and walletBalance

--- lib/portfolio_monitor/account.ex
+++ lib/portfolio_monitor/account.ex
@@ -5,6 +5,7 @@ defmodule PortfolioMonitor.Account do
   alias PortfolioMonitor.Account.BitmexAcc
   alias PortfolioMonitor.Sync
   alias PortfolioMonitor.Account.User
+  alias PortfolioMonitor.Portfolio
 
   def list_bitmex_accs do
     Repo.all(BitmexAcc)
@@ -33,5 +34,38 @@ defmodule PortfolioMonitor.Account do
     user = Pow.Plug.current_user(conn)
     query = from u in User, where: u.id == ^user.id, preload: [:bitmex_accs]
     Repo.one(query)
+  end
+
+  def bitmex_acc_with_details(user) do
+    last_available_margin =
+      from m in Portfolio.Margin,
+        where: fragment("?->>'availableMargin' IS NOT NULL", m.data),
+        order_by: [desc: m.inserted_at]
+
+    last_wallet_balance =
+      from m in Portfolio.Margin,
+        where: fragment("?->>'walletBalance' IS NOT NULL", m.data),
+        order_by: [desc: m.inserted_at]
+
+    from b in BitmexAcc,
+      join: p in assoc(b, :positions),
+      join: m in assoc(b, :margins),
+      join: mam in subquery(last_available_margin),
+      on: mam.bitmex_acc_id == b.id,
+      join: mwb in subquery(last_wallet_balance),
+      on: mwb.bitmex_acc_id == b.id,
+      where: b.user_id == ^user.id,
+      order_by: [desc: [p.inserted_at, m.inserted_at]],
+      distinct: b.id,
+      select: %{
+        id: b.id,
+        name: b.name,
+        current_qty: fragment("(?->>'currentQty')::integer", p.data),
+        liquidation_price: fragment("(?->>'liquidationPrice')::integer", p.data),
+        unrealized_pnl: fragment("(?->>'unrealisedPnl')::integer", p.data),
+        margin_balance: fragment("(?->>'marginBalance')::integer", m.data),
+        available_margin: fragment("(?->>'availableMargin')::integer", mam.data),
+        wallet_balance: fragment("(?->>'walletBalance')::integer", mwb.data)
+      }
   end
 end
