c31eeca0caa0959d8aa22c4703051b1249ec83fe
qaqaqaqaowh <nicholasowh@hotmail.com>
2020-02-04 18:20:11 +0800

    front end now makes its own ws request to get price

--- assets/js/components/SideNav.tsx
+++ assets/js/components/SideNav.tsx
@@ -14,23 +14,20 @@ const SideNav = () => {
   const [loaded, changeLoaded] = useState(false)
   const [openPrice, changeOpenPrice] = useState(1)
 
-  generalChannel.on("openingPrice", resp => {
-    console.log(resp)
-  })
-
-  // @ts-ignore
-  generalChannel.on("price", resp => {
-    loaded ? null : changeLoaded(true)
-    const newPrice: number = resp.data.data[0].price
-    changePrice(newPrice)
-  })
-
   useEffect(() => {
     const int = setInterval(() => {
       changeTime(moment())
     }, 1000)
+    const socket = new WebSocket("wss://testnet.bitmex.com/realtime?subscribe=trade:XBTUSD")
+    socket.onmessage = (data) => {
+      const parsed = JSON.parse(data.data)
+      if (parsed.table && parsed.table == "trade") {
+        changePrice(parsed.data[0].price.toFixed(1))
+      }
+    }
     return () => {
       clearInterval(int)
+      socket.close()
     }
   }, [])
 
@@ -50,7 +47,7 @@ const SideNav = () => {
       <Title level={3} style={{color: 'white'}}>{time.format("h:mm:ss A")}</Title>
       <Title level={3} style={{color: 'white', marginBottom: '20px'}}>{time.format("MMM DD, YYYY")}</Title>
       <p style={{color: 'white'}}>65.1 (0.67%)</p>
-      <Title level={3} style={{color: 'white'}}>{loaded ? price : <Spin />}</Title>
+      <Title level={3} style={{color: 'white'}}>{price ? price : <Spin />}</Title>
       <Button type="danger">Log Out</Button>
     </Sider>
   )
