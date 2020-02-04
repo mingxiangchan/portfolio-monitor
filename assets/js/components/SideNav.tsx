e1451e0e94b80adbaf6f1f3cdd53f40f42e7b320
qaqaqaqaowh <nicholasowh@hotmail.com>
2020-02-04 16:20:46 +0800

    done mockup for sidebar

--- assets/js/components/SideNav.tsx
+++ assets/js/components/SideNav.tsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import {Layout, Menu} from 'antd'
+import React, {useState, useEffect} from 'react'
+import {Layout, Menu, Typography, Button} from 'antd'
+import moment from 'moment'
 
+const { Title } = Typography;
 const {Sider} = Layout
 
-const SideNav = () => (
-  <Sider
-    width={200}
-    style={{
-      overflow: 'auto',
-      height: '100vh',
-      position: 'fixed',
-      left: 0,
-    }}
-  >
-    <Menu
-      mode="inline"
-      style={{height: '100%', borderRight: 0}}
+const SideNav = () => {
+  const [time, changeTime] = useState(moment())
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      changeTime(moment())
+    }, 1000)
+    return () => {
+      clearInterval(int)
+    }
+  }, [])
+
+  return (
+    <Sider
+      width={200}
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        textAlign: 'center',
+        padding: '20px'
+      }}
     >
-      <Menu.Item key="1">option1</Menu.Item>
-      <Menu.Item key="2">option2</Menu.Item>
-      <Menu.Item key="3">option3</Menu.Item>
-      <Menu.Item key="4">option4</Menu.Item>
-    </Menu>
-  </Sider>
-)
+      <Title level={2} style={{color: 'white'}}>Hi Josh</Title>
+      <Title level={3} style={{color: 'white'}}>{time.format("h:mm:ss A")}</Title>
+      <Title level={3} style={{color: 'white', marginBottom: '20px'}}>{time.format("MMM DD, YYYY")}</Title>
+      <p style={{color: 'white'}}>65.1 (0.67%)</p>
+      <Title level={3} style={{color: 'white'}}>9444.5</Title>
+      <Button type="danger">Log Out</Button>
+    </Sider>
+  )
+}
 
 export default SideNav
