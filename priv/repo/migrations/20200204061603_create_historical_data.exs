8383bb55e477afcce584559197b9e080388249c7
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-05 16:53:23 +0800

    only store integer for depositBtc and depositUsd

--- priv/repo/migrations/20200204061603_create_historical_data.exs
+++ priv/repo/migrations/20200204061603_create_historical_data.exs
@@ -3,12 +3,11 @@ defmodule PortfolioMonitor.Repo.Migrations.CreateHistoricalData do
 
   def change do
     create table(:historical_data) do
-      add :wallet_balance, :integer
-      add :btc_price, :integer
+      add(:wallet_balance, :integer)
+      add(:btc_price, :integer)
       add(:bitmex_acc_id, references(:bitmex_accs))
 
       timestamps()
     end
-
   end
 end
