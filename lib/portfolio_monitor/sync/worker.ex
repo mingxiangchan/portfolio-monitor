360a03a4e168bba4b218e1e1931468a59e81920f
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-06 23:22:19 +0800

    broadcast acc_id when sending ws update

--- lib/portfolio_monitor/sync/worker.ex
+++ lib/portfolio_monitor/sync/worker.ex
@@ -7,14 +7,19 @@ defmodule PortfolioMonitor.Sync.Worker do
     # spawn async, no-link process to perform db insert
     # it is acceptable to lose inserts
     Task.start(fn ->
-      persist_response(json, state[:user_id])
+      persist_response(json, Map.take(state, [:acc_id, :user_id]))
     end)
   end
 
   def persist_response(%{"action" => "partial"}, _), do: Logger.warn("ignoring partial")
 
-  def persist_response(%{"table" => table, "data" => data}, user_id) do
-    Endpoint.broadcast("bitmex_accs:#{user_id}", "ws_#{table}", %{"data" => data})
+  def persist_response(%{"table" => table, "data" => data}, state) do
+    %{user_id: user_id, acc_id: acc_id} = state
+
+    Endpoint.broadcast("bitmex_accs:#{user_id}", "ws_#{table}", %{
+      "data" => data,
+      "acc_id" => acc_id
+    })
   end
 
   def persist_response(_, _), do: nil
