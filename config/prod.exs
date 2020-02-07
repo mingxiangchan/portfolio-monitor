627d831b8cd460a56841bdf14d671e5e8d178a0e
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-07 10:26:38 +0800

    fix testnet_url for prod

--- config/prod.exs
+++ config/prod.exs
@@ -4,6 +4,8 @@ config :portfolio_monitor, PortfolioMonitorWeb.Endpoint, server: true
 
 config :logger, level: :info
 
-config :ex_bitmex, test_mode: true
+config :ex_bitmex,
+  test_mode: true,
+  domain: "testnet.bitmex.com"
 
 import_config "prod.secret.exs"
