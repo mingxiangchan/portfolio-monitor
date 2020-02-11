9ca6892735fe1bac1d3091d154f138a8ea63e4bf
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-11 10:21:40 +0800

    added detected_invalid column to bitmex_acc table

--- lib/portfolio_monitor/portfolio/bitmex_acc.ex
+++ lib/portfolio_monitor/portfolio/bitmex_acc.ex
@@ -24,6 +24,7 @@ defmodule PortfolioMonitor.Portfolio.BitmexAcc do
     field :deposit_usd, :integer
     field :deposit_btc, :integer
     field :notes, :string
+    field :detected_invalid, :boolean
     field :margin_balance, :integer, virtual: true
     field :wallet_balance_now, :integer, virtual: true
     field :wallet_balance_1_day, :integer, virtual: true
@@ -36,10 +37,20 @@ defmodule PortfolioMonitor.Portfolio.BitmexAcc do
     timestamps()
   end
 
+  @required_fields [
+    :api_key,
+    :api_secret,
+    :name,
+    :user_id,
+    :deposit_usd,
+    :deposit_btc,
+    :detected_invalid
+  ]
+
   @doc false
   def changeset(bitmex_acc, attrs) do
     bitmex_acc
-    |> cast(attrs, [:api_key, :api_secret, :name, :user_id, :deposit_usd, :deposit_btc, :notes])
-    |> validate_required([:api_key, :api_secret, :name, :user_id, :deposit_usd, :deposit_btc])
+    |> cast(attrs, @required_fields)
+    |> validate_required(@required_fields)
   end
 end
