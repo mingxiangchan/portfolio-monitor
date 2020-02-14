6b43675e6a6c3f6ebb76e85c99352bb7cce175e1
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-14 15:06:25 +0800

    added delete button and FE interactions

--- assets/js/utils/http.ts
+++ assets/js/utils/http.ts
@@ -11,10 +11,19 @@ export const doPost = (url: string, data: any, callback: (arg0: any) => void) =>
 }
 
 export const doPut = (url: string, data: any, callback: (arg0: any) => void) => {
-	axios.put(url, data, {withCredentials: true})
-	.then(callback)
-	.catch(error => {
-		const {errors} = error.response.data
-		message.error(`Error: ${JSON.stringify(errors)}`)
-	})
+  axios.put(url, data, {withCredentials: true})
+    .then(callback)
+    .catch(error => {
+      const {errors} = error.response.data
+      message.error(`Error: ${JSON.stringify(errors)}`)
+    })
+}
+
+export const doDelete = (url: string, callback: (arg0: any) => void) => {
+  axios.delete(url, {withCredentials: true})
+    .then(callback)
+    .catch(error => {
+      const {errors} = error.response.data
+      message.error(`Error: ${JSON.stringify(errors)}`)
+    })
 }
