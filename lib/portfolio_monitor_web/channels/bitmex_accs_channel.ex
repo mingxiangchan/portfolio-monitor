2239b4fab7d2d6824ba198a523332a0b63f3f917
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-17 11:44:16 +0800

    disconnect from ws on acc deletion

--- lib/portfolio_monitor_web/channels/bitmex_accs_channel.ex
+++ lib/portfolio_monitor_web/channels/bitmex_accs_channel.ex
@@ -3,7 +3,7 @@ defmodule PortfolioMonitorWeb.BitmexAccsChannel do
   use PortfolioMonitorWeb, :channel
   alias PortfolioMonitor.Portfolio
   alias PortfolioMonitor.Sync.Worker
-  intercept ["acc_update"]
+  intercept ["acc_update", "acc_deleted"]
 
   def join("bitmex_accs:index", _payload, socket) do
     topic = "bitmex_accs:#{socket.assigns.user.id}"
@@ -31,6 +31,15 @@ defmodule PortfolioMonitorWeb.BitmexAccsChannel do
     {:noreply, updated_socket}
   end
 
+  def handle_out("acc_deleted", msg, socket) do
+    %{acc: %{id: id}} = msg
+    updated_socket = kill_ws_worker(socket, id)
+    push(updated_socket, "acc_deleted", msg)
+    {:noreply, updated_socket}
+  end
+
+  # Endpoint.broadcast("bitmex_accs:#{acc.user_id}", "acc_deleted", %{acc: %{id: acc.id}})
+
   # PRIVATE APIS
 
   defp initialize_ws_workers(socket) do
@@ -46,26 +55,41 @@ defmodule PortfolioMonitorWeb.BitmexAccsChannel do
     assign(socket, :existing_workers, updated_workers)
   end
 
+  defp kill_ws_worker(socket, id) do
+    existing_workers = Map.get(socket.assigns, :existing_workers, %{})
+
+    case Map.get(existing_workers, id) do
+      nil ->
+        socket
+
+      pid ->
+        Process.exit(pid, :normal)
+        updated_workers = Map.drop(existing_workers, [id])
+        assign(socket, :existing_workers, updated_workers)
+    end
+  end
+
   defp start_acc_ws_worker(%{detected_invalid: true}, _), do: nil
 
   defp start_acc_ws_worker(acc, existing_workers) do
     auth_config = Map.take(acc, [:api_key, :api_secret])
-    existing_pid = Map.get(existing_workers, acc.id)
 
-    if is_nil(existing_pid) do
-      {:ok, pid} =
-        Worker.start_link(%{
-          acc_id: acc.id,
-          user_id: acc.user_id,
-          auth_subscribe: ["margin", "position"],
-          is_testnet: acc.is_testnet,
-          config: auth_config,
-          name: String.to_atom("BitMexAccWorker.#{acc.id}")
-        })
+    case Map.get(existing_workers, acc.id) do
+      nil ->
+        {:ok, pid} =
+          Worker.start_link(%{
+            acc_id: acc.id,
+            user_id: acc.user_id,
+            auth_subscribe: ["margin", "position"],
+            is_testnet: acc.is_testnet,
+            config: auth_config,
+            name: String.to_atom("BitMexAccWorker.#{acc.id}")
+          })
 
-      {acc.id, pid}
-    else
-      {acc.id, existing_pid}
+        {acc.id, pid}
+
+      existing_pid ->
+        {acc.id, existing_pid}
     end
   end
 end
