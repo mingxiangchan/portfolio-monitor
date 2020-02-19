9f6d1c7f8e7662108d9cd55b19dfb9baee417ef1
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-19 13:02:20 +0800

    added spinners

--- assets/js/components/AccCardOverview.tsx
+++ assets/js/components/AccCardOverview.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Card, Row, Col, Typography, Descriptions } from 'antd'
+import { Card, Row, Col, Typography, Descriptions, Icon } from 'antd'
 import { BitmexAcc } from '../types'
 
-const { Title, Paragraph } = Typography
+const { Title, Paragraph, Text } = Typography
 
 interface PropTypes {
   acc: BitmexAcc
@@ -11,18 +11,45 @@ interface PropTypes {
   livePrice: number
 }
 
+const isLoaded = (val): boolean => val !== null && val !== undefined
+
+const spinner = <Text disabled>NA</Text>
+
 const AccCardOverview = ({
   acc,
   btcBalance,
   usdBalance,
   livePrice,
 }: PropTypes) => {
-  const leverage = Math.abs(acc.currentQty / usdBalance).toFixed(2)
-  const liquidationDistanceAbs = acc.liquidationPrice - livePrice
-  const liquidationDistancePer = (
-    (liquidationDistanceAbs / acc.liquidationPrice) *
-    100
-  ).toFixed(2)
+  const openPos = isLoaded(acc.currentQty)
+    ? `${acc.currentQty} / BTC ${(acc.currentQty / livePrice).toFixed(4)}`
+    : spinner
+
+  const leverage =
+    isLoaded(acc.currentQty) && isLoaded(usdBalance)
+      ? `${Math.abs(acc.currentQty / usdBalance).toFixed(2)} x`
+      : spinner
+
+  const liquidationPrice = isLoaded(acc.liquidationPrice)
+    ? acc.liquidationPrice
+    : spinner
+
+  const liquidationDistanceAbs =
+    isLoaded(acc.liquidationPrice) && isLoaded(livePrice)
+      ? acc.liquidationPrice - livePrice
+      : spinner
+
+  const liquidationDistancePer =
+    isLoaded(acc.liquidationPrice) && isLoaded(livePrice)
+      ? ((liquidationDistanceAbs / acc.liquidationPrice) * 100).toFixed(2)
+      : spinner
+
+  const avgEntryPrice = isLoaded(acc.avgEntryPrice)
+    ? `USD ${acc.avgEntryPrice}`
+    : spinner
+
+  const notes = isLoaded(acc.notes) ? acc.notes : spinner
+
   return (
     <Card>
       <Row>
@@ -36,13 +63,13 @@ const AccCardOverview = ({
       <Row>
         <Col span={16}>
           <Title level={4} style={{ textAlign: 'center' }}>
-            {acc.currentQty} / BTC {(acc.currentQty / livePrice).toFixed(4)}
+            {openPos}
           </Title>
           <p style={{ textAlign: 'right' }}>Open Position</p>
         </Col>
         <Col span={8}>
           <Title level={4} style={{ textAlign: 'center' }}>
-            {leverage}x
+            {leverage}
           </Title>
           <p style={{ textAlign: 'right' }}>Leverage</p>
         </Col>
@@ -51,15 +78,15 @@ const AccCardOverview = ({
         <Col span={24}>
           <Descriptions column={1}>
             <Descriptions.Item label="Liquidation">
-              {acc.liquidationPrice} / {liquidationDistanceAbs} /{' '}
+              {liquidationPrice} / {liquidationDistanceAbs} /{' '}
               {liquidationDistancePer} %
             </Descriptions.Item>
             <Descriptions.Item label="Avg Entry Price">
-              USD {acc.avgEntryPrice}
+              {avgEntryPrice}
             </Descriptions.Item>
             <Descriptions.Item label="Notes">
               <Paragraph ellipsis={{ rows: 2, expandable: true }}>
-                {acc.notes ? acc.notes : 'NA'}
+                {notes}
               </Paragraph>
             </Descriptions.Item>
           </Descriptions>
