c4cc2cdbb5926f8bd8d9b54f2a560daba59dba59
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-19 10:18:39 +0800

    temp

---
+++ assets/js/components/AccCardOverview.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Card, Row, Col } from 'antd'
+
+const AccCardOverview = () => {
+  return (
+    <Card>
+      <Row>
+        <Col span={24}></Col>
+      </Row>
+      <Row>
+        <Col span={16}></Col>
+        <Col span={8}></Col>
+      </Row>
+      <Row>
+        <Col span={24}></Col>
+      </Row>
+    </Card>
+  )
+}
+
+export default AccCardOverview
