0607536470ba0f1008f059176b576e133a1c0434
Ming Xiang Chan <mingxiangchan@gmail.com>
2020-02-25 14:30:41 +0800

    change to display live by default

--- assets/js/context/DashboardContext.tsx
+++ assets/js/context/DashboardContext.tsx
@@ -5,7 +5,7 @@ const DashboardContext = React.createContext(null)
 const DashboardContextProvider = ({
   children,
 }: React.PropsWithChildren<{}>) => {
-  const [testnet, setTestnet] = useState(true)
+  const [testnet, setTestnet] = useState(false)
   return (
     <DashboardContext.Provider value={{ testnet, setTestnet }}>
       {children}
